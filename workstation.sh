c1d4d7b511b8702b31c8c8214c5629c22b75002c
daws-86ss <arjunmsp123@gmail.com>
2026-01-04 11:11:40 +0530

    k8s

--- workstation.sh
+++ workstation.sh
@@ -13,6 +13,7 @@ curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linu
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 
+
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
